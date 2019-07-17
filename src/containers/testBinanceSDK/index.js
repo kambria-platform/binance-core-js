fe8c13c77047cae9a219ba659ef709d7c8d958ac
tphanson <tphanson@kambria.io>
2019-07-17 16:11:29 +0700

    complete binance-sdk

--- src/containers/testBinanceSDK/index.js
+++ src/containers/testBinanceSDK/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BinanceSDK } from 'binance-core-js';
 
-const MNEMONIC = 'family exact strategy quote about step magic steel afraid cage remain vintage';
+const PRIVATE_KEY = '485647f8e0cdbe18865495847e0d0bd8a25b687804344aefb266e2bd369d4d27';
+const MNEMONIC = 'page term erupt print mystery sell cup purse arrow term corn couch inquiry parrot curtain clown owner daughter opera bleak february oven chat saddle';
+const KEYSTORE = require('./5f80e75e-d947-4971-b2dc-45cd6b63c725_keystore.json');
 
 const DEFAULT_STATE = {
   network: null,
@@ -16,17 +18,43 @@ class TestBinanceSDK extends Component {
     this.state = { ...DEFAULT_STATE };
   }
 
-  connectByMnemonic = () => {
-    this.binanceSDK = new BinanceSDK(4, 'softwallet', true);
-    this.binanceSDK.setAccountByMnemonic(
-      MNEMONIC,
-      null,
-      "m/44'/714'/0'/0",
-      1,
+  connectByPrivatekey = () => {
+    this.binanceSDK = new BinanceSDK(2, 'softwallet', true);
+    this.binanceSDK.setAccountByPrivatekey(
+      PRIVATE_KEY,
       cb => cb(null, 'dummy passphrase'),
       (er, client) => {
         if (er) return console.error(er);
-        console.log(client)
+        this.watcher = this.binanceSDK.watch((er, re) => {
+          if (er) return console.error(er);
+          this.setState(re);
+        });
+      });
+  }
+
+  connectByMnemonic = () => {
+    this.binanceSDK = new BinanceSDK(2, 'softwallet', true);
+    this.binanceSDK.setAccountByMnemonic(
+      MNEMONIC,
+      0,
+      cb => cb(null, 'dummy passphrase'),
+      (er, client) => {
+        if (er) return console.error(er);
+        this.watcher = this.binanceSDK.watch((er, re) => {
+          if (er) return console.error(er);
+          this.setState(re);
+        });
+      });
+  }
+
+  connectByKeystore= () => {
+    this.binanceSDK = new BinanceSDK(2, 'softwallet', true);
+    this.binanceSDK.setAccountByKeystore(
+      KEYSTORE,
+      'ASDqwe12#',
+      cb => cb(null, 'dummy passphrase'),
+      (er, client) => {
+        if (er) return console.error(er);
         this.watcher = this.binanceSDK.watch((er, re) => {
           if (er) return console.error(er);
           this.setState(re);
@@ -35,19 +63,32 @@ class TestBinanceSDK extends Component {
   }
 
   sendTx = () => {
-    if (this.watcher) this.watcher.stopWatching();
+    if (this.binanceSDK) this.binanceSDK.client.transfer(
+      this.state.account,
+      this.state.account,
+      1, 'BNB'
+    ).then(re => {
+      console.log(re.result[0].hash);
+    }).catch(er => {
+      console.error(er);
+    });
   }
 
   logout = () => {
     if (this.binanceSDK) this.binanceSDK.logout();
   }
 
+  componentWillUnmount() {
+    if (this.watcher) this.watcher.stopWatching();
+  }
 
   render() {
     return (
       <div>
-        <h1>Isoxys Test</h1>
+        <h1>BinanceSDK Test</h1>
+        <button onClick={this.connectByPrivatekey}>Connect by Privatekey</button>
         <button onClick={this.connectByMnemonic}>Connect by Mnemonic</button>
+        <button onClick={this.connectByKeystore}>Connect by Keystore</button>
         <button onClick={this.sendTx}>Send</button>
         <button onClick={this.logout}>Logout</button>
         <p>Network: {this.state.network}</p>
